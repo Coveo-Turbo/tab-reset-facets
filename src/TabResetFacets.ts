9726711ec5c71ef909a7fee22311d282e6d2a1d9
Julian Wolfe <jwolfe@coveo.com>
2021-02-16 12:13:51 -0500

    Accept SimpleFilter, Facet, DynamicFacet

--- src/TabResetFacets.ts
+++ src/TabResetFacets.ts
@@ -1,39 +1,51 @@
-import { $$, Component, IComponentBindings, ComponentOptions, IAttributeChangedEventArg, Facet, QueryStateModel, DynamicFacet } from 'coveo-search-ui';
-import { lazyComponent } from '@coveops/turbo-core';
+import { $$, Component, IComponentBindings, ComponentOptions, IAttributeChangedEventArg } from 'coveo-search-ui';
+import { lazyDependentComponent } from '@coveops/turbo-core';
 
 export interface ITabResetFacetsOptions { }
 
-@lazyComponent
+@lazyDependentComponent('Facet')
 export class TabResetFacets extends Component {
     static ID = 'TabResetFacets';
+
     static options: ITabResetFacetsOptions = {};
 
     constructor(public element: HTMLElement, public options: ITabResetFacetsOptions, public bindings: IComponentBindings) {
         super(element, TabResetFacets.ID, bindings);
+
         this.options = ComponentOptions.initComponentOptions(element, TabResetFacets, options);
 
         // State Events
-        let changeTabEvtName = this.getStateEventName(QueryStateModel.eventTypes.changeOne + QueryStateModel.attributesEnum.t);
+        let changeTabEvtName = this.getStateEventName(Coveo.QueryStateModel.eventTypes.changeOne + Coveo.QueryStateModel.attributesEnum.t);
 
         this.bind.onRootElement(changeTabEvtName, (args: IAttributeChangedEventArg) => this.handleTabStateChanged(args));
     }
 
     private getStateEventName(event: string) {
-        return QueryStateModel.ID + ':' + event;
+        return Coveo.QueryStateModel.ID + ':' + event;
     }
 
     private resetCoveoFacets() {
         const facets = [
-            ...$$(document.body).findClass(`Coveo${DynamicFacet.ID}`),
-            ...$$(document.body).findClass(`Coveo${Facet.ID}`),
+            ...$$(document.body).findClass('CoveoFacet'),
+            ...$$(document.body).findClass('CoveoDynamicFacet'),
+            ...$$(document.body).findClass('CoveoSimpleFilter'),
         ]
 
         facets.forEach(facet => {
-            // The DynamicFacet and Facet don't directly share the same interface so casting for basic.
-            let facetObj = <Facet>Component.get(facet);
+            let facetObj = Component.get(facet);
 
-            if (facetObj) {
-                facetObj.reset();
+            // Manually handling inconsistent API between filters and facets.
+
+            if (facetObj && 'Facet' === facetObj.type) {
+                (<Coveo.Facet>facetObj).reset();
+            }
+
+            if (facetObj && 'DynamicFacet' === facetObj.type) {
+                (<Coveo.DynamicFacet>facetObj).reset();
+            }
+
+            if (facetObj && 'SimpleFilter' === facetObj.type) {
+                (<Coveo.SimpleFilter>facetObj).resetSimpleFilter();
             }
         });
     }
